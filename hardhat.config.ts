d6930b8e64b6cec88587da1d00ecbefb2b4436df
KStasi <nastasiakondaurova@gmail.com>
2023-11-15 10:00:44 +0200

    init project

---
+++ hardhat.config.ts
@@ -0,0 +1,17 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+const dotenv = require("dotenv");
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+  networks: {
+    goerli: {
+      url: "https://goerli.infura.io/v3/" + process.env.INFURA_KEY,
+      accounts: [process.env.PRIVATE_KEY || ""],
+      chainId: 5,
+    },
+  },
+};
+
+export default config;
